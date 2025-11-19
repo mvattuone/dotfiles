45cfce5ff84e0cf0cbf205249e042ac09c675c7b
Michael Vattuone <mvattuone@gmail.com>
2025-11-19 03:38:23 -0500

    Ladd lazy.nvim bootstrap

--- .config/nvim/lua/core.lua
+++ .config/nvim/lua/core.lua
@@ -1,8 +1,24 @@
 -----------------------------------------------------------
------------------------------------------------------------
 -- Leader key
 -----------------------------------------------------------
 vim.g.mapleader = " "
+
+-----------------------------------------------------------
+-- Lazy.nvim bootstrap
+-----------------------------------------------------------
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git", "clone", "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  { import = "plugins" },
+})
 
 -----------------------------------------------------------
 -- Core UI behavior
