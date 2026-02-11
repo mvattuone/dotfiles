f94a2213dd6bbd2fd2aa2a96c4f52570b1638b46
Michael Vattuone <mvattuone@gmail.com>
2026-02-11 01:24:03 -0500

    VSCode like formatting for autocomplete options via lspkind

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -1,3 +1,4 @@
+local lspkind = require('lspkind')
 local cmp = require('cmp')
 
 lsp.preset('recommended')
@@ -28,8 +29,34 @@ cmp.setup {
       hl_group = "Comment",
     },
   },
+  formatting = {
+      fields = { 'kind', 'abbr', 'menu' },
+      expandable_indicator = true,
+      format = lspkind.cmp_format({
+        maxwidth = {
+          -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+          -- can also be a function to dynamically calculate max width such as
+          -- menu = function() return math.floor(0.45 * vim.o.columns) end,
+          menu = 50, -- leading text (labelDetails)
+          abbr = 50, -- actual suggestion item
+        },
+        mode = 'symbol',
+        ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+        show_labelDetails = true, -- show labelDetails in menu. Disabled by default
 
-lsp.ensure_installed({
+        -- The function below will be called before any actual modifications from lspkind
+        -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+        before = function (entry, vim_item)
+          local menu_icon = {
+            nvim_lsp = '[LSP]',
+            buffer = '[Buf]',
+            path = '[Path]',
+          }
+          vim_item.menu = menu_icon[entry.source.name] or entry.source.name
+          return vim_item
+        end
+      })
+    }
 }
 
 cmp.setup.filetype('gitcommit', {
