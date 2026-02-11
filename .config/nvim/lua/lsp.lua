15dee4bd417c16b6327cdd895669b3cb96fa6df5
Michael Vattuone <mvattuone@gmail.com>
2026-02-11 01:23:09 -0500

    Use native LSP and remove now deprecated lsp-zero

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -1,23 +1,81 @@
-local lsp = require('lsp-zero')
+local cmp = require('cmp')
 
 lsp.preset('recommended')
+cmp.setup {
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'path' },
+    { name = 'buffer' },
+  },
+  window = {
+    completion = {
+      border = 'rounded',
+      scrollbar = false,
+      winhighlight = 'Normal:Normal,FloatBorder:FloatBorder,CursorLine:Visual,Search:None',
+    },
+    documentation = {
+      border = 'rounded',
+      winhighlight = 'Normal:Normal,FloatBorder:FloatBorder,CursorLine:Visual,Search:None',
+    },
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-n>'] = cmp.mapping.select_next_item(),
+    ['<C-p>'] = cmp.mapping.select_prev_item(),
+    ['<Tab>'] = cmp.mapping.confirm({ select = true }),
+  }),
+  experimental = {
+    ghost_text = {
+      hl_group = "Comment",
+    },
+  },
 
 lsp.ensure_installed({
+}
+
+cmp.setup.filetype('gitcommit', {
+sources = cmp.config.sources({
+  { name = 'git' },
+}, {
+  { name = 'buffer' },
+})
+})
+require("cmp_git").setup()
+
+local cmp_capabilities = require('cmp_nvim_lsp').default_capabilities()
+local servers = {
   'html',
   'cssls',
   'ts_ls',
   'eslint',
   'pyright',
   'rust_analyzer',
-})
+  'lua_ls',
+}
 
-require('lspconfig').ts_ls.setup({
-  on_attach = function(client, bufnr)
-  end,
+local function with_capabilities(opts)
+  return vim.tbl_deep_extend('force', { capabilities = cmp_capabilities }, opts or {})
+end
+
+for _, server in ipairs(servers) do
+  if server ~= 'ts_ls' and server ~= 'lua_ls' then
+    vim.lsp.config(server, with_capabilities())
+  end
+end
+
+vim.lsp.config('ts_ls', with_capabilities({
   settings = {
-    defaultMaximumTruncationLength = 800
+    defaultMaximumTruncationLength = 800,
   },
-})
+}))
 
-lsp.nvim_workspace()
-lsp.setup()
+vim.lsp.config("lua_ls", with_capabilities({
+  cmd = { "lua-language-server" },
+  filetypes = { "lua" },
+  root_markers = { { ".luarc.json", ".luarc.jsonc" }, ".git" },
+  settings = {
+    Lua = {
+      runtime = { version = "LuaJIT" },
+    },
+  },
+}))
+
